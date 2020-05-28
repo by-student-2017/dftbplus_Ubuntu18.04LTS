a8520e622dfefa969f05e3326b6b59a3eeec13e1
by-student-2017 <studentsctest@gmail.com>
2020-05-28 18:57:24 +0900

    OpenMPI+OpenBlas

---
+++ README.txt
@@ -0,0 +1,27 @@
+------------------------------------------------------------------------------
+DFTB+ 19.1 (ubuntu 18.04 LTS)
+
+Installation
+1. cp dftbplus-19.1.tar.gz $HOME/dftbplus-19.1.tar.gz
+  cp install-19.1.sh $HOME/install-19.1.sh
+2. cd $HOME
+3. install-19.1.sh
+4. echo "" >> ~/.bashrc
+  echo "# dftb+ 19.1" >> ~/.bashrc
+  echo 'export PATH=$PATH:$HOME/dftbplus-19.1/_install/bin' >> ~/.bashrc
+  echo 'export PATH=$PATH:$HOME/dftbplus-19.1/tools/misc' >> ~/.bashrc
+5. bash
+------------------------------------------------------------------------------
+DFTB+ 18.2 (ubuntu 18.04 LTS)
+
+Installation
+1. cp dftbplus-18.2.tar.gz $HOME/dftbplus-18.2.tar.gz
+  cp install-18.2.sh $HOME/install-18.2.sh
+2. cd $HOME
+3. install-18.2.sh
+4. echo "" >> ~/.bashrc
+  echo "# dftb+ 18.2" >> ~/.bashrc
+  echo 'export PATH=$PATH:$HOME/dftbplus-18.2/_install/bin' >> ~/.bashrc
+  echo 'export PATH=$PATH:$HOME/dftbplus-18.2/tools/misc' >> ~/.bashrc
+5. bash
+------------------------------------------------------------------------------
---
+++ install-18.2.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+num_core=`grep 'core id' /proc/cpuinfo | sort -u | wc -l`
+mflags=`grep 'flags' /proc/cpuinfo`
+for i in $mflags; do
+  if [ $i = "avx2" ] && [ $((p)) -lt 6 ]; then
+    mop="avx2"
+    p=6
+  fi
+  if [ $i = "avx" ] && [ $((p)) -lt 5 ]; then
+    mop="avx"
+    p=5
+  fi
+  if [ $i = "sse4_2" ] && [ $((p)) -lt 4 ]; then
+    mop="sse4.2"
+    p=4
+  fi
+  if [ $i = "sse4_1" ] && [ $((p)) -lt 3 ]; then
+    mop="sse4.1"
+    p=3
+  fi
+  if [ $i = "ssse3" ] && [ $((p)) -lt 2 ]; then
+    mop="ssse3"
+    p=2
+  fi
+  if [ $i = "sse2" ] && [ $((p)) -lt 2 ]; then
+    mop="sse2"
+    p=1
+  fi
+done
+echo "number of cups: "$num_core
+echo "tuning option: -m"$mop
+
+#
+if [ -d ./dftbplus-18.2 ];
+then
+  echo "detect old dftbplus-18.2 directory !"
+  echo "delet old dftbplus-18.2 directory"
+  sudo rm -f -r ./dftbplus-18.2
+fi
+#
+
+echo "++++++++++download++++++++++"
+sudo apt update
+sudo apt install -y unzip
+sudo apt install -y g++
+sudo apt install -y gcc
+sudo apt install -y bulid-essential
+sudo apt install -y gfortran
+sudo apt install -y libopenmpi-dev
+sudo apt install -y m4
+sudo apt install -y libscalapack-openmpi-dev
+sudo apt install -y libscalapack-openmpi2.0
+sudo apt install -y libopenblas-dev
+sudo apt install -y liblapack-dev
+sudo apt install -y libarpack2-dev
+sudo apt install -y python3.7
+sudo apt install -y libpython3.7-dev
+sudo apt install -y python3.7-distutils
+sudo apt install -y python-numpy
+sudo apt install -y python-matplotlib
+sudo apt install -y csh
+sudo apt install -y wget
+sudo apt install -y git
+sudo apt install -y make
+sudo apt install -y cmake
+sudo apt install -y jmol
+#sudo apt install -y grace
+#sudo apt install -y gnuplot
+#sudo apt install -y nkf
+#sudo apt install -y libfftw3-dev
+#sudo apt install -y fftw-dev
+#sudo apt install -y python
+#sudo apt install -y python2
+#sudo apt install -y libpython2-dev
+
+echo "++++++++++unpack++++++++++"
+tar zxvf dftbplus-18.2.tar.gz
+cd dftbplus-18.2
+#git submodule update --init --recursive
+#./utils/get_opt_externals ALL
+
+echo "++++++++++compiling++++++++++"
+#mkdir _build
+#cd _build
+#cmake -DWITH_DFTD3=true -DWITH_TRANSPORT=true -DFYPP_FLAGS="-DTRAVIS" -DWITH_ARPACK=true -DCMAKE_TOOLCHAIN_FILE=../sys/gnu.cmake ..
+sed -i "s/avx2/$mop/g" make.arch
+make -j${num_core}
+make test TEST_MPI_PROCS=${num_core} TEST_OMP_THREADS=2 
+make install
+
+echo "++++++++++dptools setting++++++++++"
+cd ~/dftbplus-18.2/tools/dptools
+sudo python3 setup.py install
+
+echo "++++++++++tests++++++++++"
+#cd ~/dftbplus-18.2/_build
+#ctest
